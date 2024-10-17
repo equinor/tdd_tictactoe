00a88b57b71b7c4bf9b869cef1c3508493820526
fredrir <fhansteen@gmail.com>
2024-10-17 17:44:19 +0200

    create tictactoe game

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,9 +1,51 @@
 ﻿class TicTacToe:
+    
     def __init__(self):
         self.board =  "I AM A BOARD"
+        self.current_player = "X"
+              
+        grid = 3
+        board  = []
+        
+        for i in range(grid):
+            board.append([])
+            for j in range(grid):
+                board[i].append(" ")
 
-
-
+        self.board = board
+        
+        
+    def switch_player(self):
+        if self.current_player == "X":
+            self.current_player = "O"
+        else:
+            self.current_player = "X"
+        
+        return self.current_player
+    
+    
+    def make_move(self, row, col):
+        if self.board[row][col] == " ":
+            self.board[row][col] = self.current_player
+            self.switch_player()
+            return True
+        else:
+            return False
+        
+        
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != " ":
+                return self.board[i][0]
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != " ":
+                return self.board[0][i]
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
+            return self.board[0][0]
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
+            return self.board[0][2]
+        return None
+    
+    
 
 if __name__ == '__main__':
     game = TicTacToe()
--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -7,6 +7,38 @@ class TestTicTacToe(unittest.TestCase):
         expected_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         self.assertEqual(game.board, expected_board, "The board should initialize as a 3x3 grid of empty strings")
         print("test_board:initialization passed")
+        
+        
+        
+    def test_player_switch(self):
+        game = TicTacToe()
+        self.assertEqual(game.current_player, "X", "The starting player should be X")
+        game.switch_player()
+        self.assertEqual(game.current_player, "O", "After switching, the current player should be O")
+        game.switch_player()
+        self.assertEqual(game.current_player, "X", "After switching again, the current player should be X")
+        print("test_player_switch passed")
+        
+        
+    def test_make_move(self):
+        game = TicTacToe()
+        self.assertEqual(game.make_move(0, 0), True, "The first move should be valid")
+        self.assertEqual(game.make_move(0, 0), False, "The same move should be invalid")
+        self.assertEqual(game.make_move(0, 1), True, "A different move should be valid")
+        self.assertEqual(game.make_move(0, 1), False, "The same move should be invalid")
+        print("test_make_move passed")
+        
+        
+    def test_check_winner(self):
+        game = TicTacToe()
+        self.assertEqual(game.check_winner(), None, "There should be no winner at the start of the game")
+        game.make_move(0, 0)
+        game.make_move(1, 0)
+        game.make_move(0, 1)
+        game.make_move(1, 1)
+        game.make_move(0, 2)
+        self.assertEqual(game.check_winner(), "X", "X should win with a top row")
+        print("test_check_winner passed")
 
 if __name__ == '__main__':
     unittest.main()
